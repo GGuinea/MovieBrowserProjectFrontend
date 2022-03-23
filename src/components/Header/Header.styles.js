193b30899077e1d7e5c7d3d9f089be5049dad016
Gguinea <Gguinea>
2022-03-24 00:17:32 +0100

    Styling header

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Wrapper= styled.div`
+background: var(--darkGrey);
+padding: 0 20px;
+`;
+
+export const Content = styled.div`
+display: flex;
+align-items: content;
+
+`;
+
+export const LogoImg = styled.img``;
+
+export const TMDBLogoImg = styled.img`
+`;
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {Wrapper, Content } from './Header.styles';
+
+const Header = () => (
+    <Wrapper>
+        <Content>
+            Hello there
+        </Content>
+    </Wrapper>
+);
+export default Header;
+
