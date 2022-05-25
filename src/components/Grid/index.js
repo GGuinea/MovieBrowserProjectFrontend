2f279bee05edbda4bb9c63fd96adaec6057eb227
Gguinea <kamil5176@gmail.com>
2022-05-25 21:26:54 +0200

    Thumbs

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 
-import {Wrapper, Content } from './Grid.styles';
+import { Wrapper, Content } from "./Grid.styles";
 
-const Grid = ({header, children}) => (
-    <Wrapper>
-        <h1>
-            {header}
-        </h1>
-            <Content>
-                {children}
-                </Content>
-    </Wrapper>
-)
+const Grid = ({ header, children }) => (
+  <Wrapper>
+    <h1>{header}</h1>
+    <Content>{children}</Content>
+  </Wrapper>
+);
 
 export default Grid;
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,28 +8,37 @@ import {useMoveFetch} from '../hooks/useHomeFetch'
 import NoImage from '../images/no_image.jpg';
 import Grid from './Grid';
 import HeroImage from './HeroImage';
+import Thumb from './Thumb';
 
 
 const Home = () => {
     const {state, loading, error} = useMoveFetch();
     console.log(state);
     return (
-        <>
-            {state.results[0] ?
-                <HeroImage
-                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-                    title={state.results[0].original_title}
-                    text={state.results[0].overview}
-                />
-            : null
-            }
-        <Grid header='Popular Mobies'>
-            {state.results.map(movie=>(
-                <div key={movie.id}>{movie.title}</div>
-            ))}
+      <>
+        {state.results[0] ? (
+          <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+            title={state.results[0].original_title}
+            text={state.results[0].overview}
+          />
+        ) : null}
+        <Grid header="Popular Mobies">
+          {state.results.map((movie) => (
+            <Thumb
+              key={movie.id}
+              clickable
+              image={
+                movie.poster_path
+                  ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                  : NoImage
+              }
+              movieId={movie.id}
+            ></Thumb>
+          ))}
         </Grid>
-        </>
-    )
+      </>
+    );
 };
 
 export default Home;
