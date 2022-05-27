0028ff33af2c01a6bae9f296cd6df209ad1b1733
Gguinea <kamil5176@gmail.com>
2022-05-27 11:21:24 +0200

    Info

---
+++ src/components/MovieInfoBar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {calcTime, convertMoney} from '../../helpers/';
+
+import {Wrapper, Content} from './MovieInfoBar.styles'
+
+const MovieInfoBar = ({time, budget, revenue}) => (
+    <Wrapper>
+        <Content>
+            <div className="column">
+
+                </div>
+            </Content>
+    </Wrapper>
+)
+
+export default MovieInfoBar;
