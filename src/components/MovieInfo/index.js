2997dfe78f7c759e5161c6f917e5870999eb1314
Gguinea <kamil5176@gmail.com>
2022-05-27 13:28:25 +0200

    Nice features

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -1,39 +1,50 @@
-import React from 'react';
-import { Wrapper, Content, Text } from './MovieInfo.styles';
-import Thumb from '../Thumb';
-import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
-import NoImage from '../../images/no_image.jpg'
-import Button from '../Button/';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Wrapper, Content, Text } from "./MovieInfo.styles";
+import Thumb from "../Thumb";
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../../config";
+import NoImage from "../../images/no_image.jpg";
+import Button from "../Button/";
+import { Context } from "../../context";
 
-const MovieInfo = ({ movie }) => (
-  <Wrapper backdrop={movie.backdrop_path}>
-    <Content>
-      <Thumb
-        image={
-          movie.poster_path
-            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
-            : NoImage
-        }
-        clickable={false}
-        alt="movie-thumb"
-      ></Thumb>
-      <Text>
-        <h1>{movie.title}</h1>
-        <h3>PLOT</h3>
-        <p>{movie.overview}</p>
+const MovieInfo = ({ movie }) => {
+  const [user] = useContext(Context);
+  return (
+    <Wrapper backdrop={movie.backdrop_path}>
+      <Content>
+        <Thumb
+          image={
+            movie.poster_path
+              ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+              : NoImage
+          }
+          clickable={false}
+          alt="movie-thumb"
+        ></Thumb>
+        <Text>
+          <h1>{movie.title}</h1>
+          <h3>PLOT</h3>
+          <p>{movie.overview}</p>
 
-        <div className="rating-direcotrs">
-          <div>
-            <h3>RATING</h3>
-            <div className="score">{movie.vote_average}</div>
+          <div className="rating-direcotrs">
+            <div>
+              <h3>RATING</h3>
+              <div className="score">{movie.vote_average}</div>
+            </div>
+            <div className="director">
+              {user ? (
+                <Button text="Add to favorite"></Button>
+              ) : (
+                <Link to="/login">
+                  <Button text="Log In"></Button>
+                </Link>
+              )}
+            </div>
           </div>
-          <div className="director">
-            <Button text="Dodac tutaj metode asyn dodajaca do ulubionych dla klienta"></Button>
-          </div>
-        </div>
-      </Text>
-    </Content>
-  </Wrapper>
-);
+        </Text>
+      </Content>
+    </Wrapper>
+  );
+};
 
 export default MovieInfo;
